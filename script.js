3078746b971101c72b32145ddfec2e44d5876811
Ricardo <ricoazeredo@yahoo.com.br>
2025-10-03 20:07:59 -0300

    add javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+window.alert("Iniciou o js"); 
+    let logo = document.getElementById("logo");
+    console.log(logo);
+    document.getElementById("logo").innerHTML = "Ricardo";
+    document.querySelector('aside').style = "background-color: #ea07e4";
